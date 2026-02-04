de194fca8b1b02aac37f2f4fcdc8b48ef5b9431c
irispsyence <irispsyence@gmail.com>
2026-02-04 13:58:19 -0500

    update draw player

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
-
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     #print("Hello from asteroidsboot!")
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -15,12 +18,15 @@ def main():
     while True:
         log_state()
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            return
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
 
-    screen.fill("black")
-    pygame.display.flip()
+        screen.fill("black")
+        player.draw(screen)
+        pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
